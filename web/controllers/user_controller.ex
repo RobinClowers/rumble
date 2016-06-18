0e36a766e245b10839a7b728c81cfa4849bdedcc
Robin Clowers <robin.clowers@gmail.com>
2016-06-18 12:12:27 -0700

    Add users index and show

---
+++ web/controllers/user_controller.ex
@@ -0,0 +1,15 @@
+defmodule Rumble.UserController do
+  use Rumble.Web, :controller
+  alias Rumble.Repo
+  alias Rumble.User
+
+  def index(conn, _params) do
+    users = Repo.all(User)
+    render conn, "index.html", users: users
+  end
+
+  def show(conn, %{"id" => id}) do
+    user = Repo.get(User, id)
+    render conn, "show.html", user: user
+  end
+end
