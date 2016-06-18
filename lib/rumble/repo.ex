fdeb0511b6f9c7e5d1972e48dd89a0d1d115f6d1
Robin Clowers <robin.clowers@gmail.com>
2016-06-18 11:04:40 -0700

    Add Repo.all

--- lib/rumble/repo.ex
+++ lib/rumble/repo.ex
@@ -1,3 +1,16 @@
 defmodule Rumble.Repo do
-  use Ecto.Repo, otp_app: :rumble
+  alias Rumble.User
+
+  @moduledoc """
+  In memory repository.
+  """
+
+  def all(Rumble.User) do
+    [
+      %User{id: "1", name: "José", username: "josevalim", password: "elixir"},
+      %User{id: "2", name: "Bruce", username: "redrapids", password: "7langs"},
+      %User{id: "3", name: "Chris", username: "chrismccord", password: "phx"}
+    ]
+  end
+  def all(_module), do: []
 end
